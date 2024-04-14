23e7d20879303706bec1e880f2f8b744d167113c
lnardon <lucaslnardon@gmail.com>
2024-04-14 20:54:41 -0300

    ✨ Add decrypt and find key

--- cypher.go
+++ cypher.go
@@ -1,13 +1,13 @@
 package main
 
-func IsLetter(char byte){
+func IsLetter(char byte) bool {
         if char >= 65 && char <= 90 {
                 return true
         }
         return false
 }
 
-func DecryptChar (textChar byte, keyChar byte) {
+func DecryptChar (textChar byte, keyChar byte) string {
         dc := (textChar - keyChar) % 26
 
         for dc < 0 {
@@ -17,15 +17,17 @@ func DecryptChar (textChar byte, keyChar byte) {
         return string(dc + 65)
 }
 
-func EncryptChar (textChar byte, keyChar byte){
-        ec := ((textChar - 65) + (keyChar - 65)) % 26
-        return string(ec + 65)
-}
+func DecryptText(txt string, key string) string {
+	res := ""
+	k_idx := 0
+	c := 0
+	for i := 0; i < len(txt); i++ {
+                if IsLetter(txt[i]) {
+                        k_idx = c % len(key)
+                        res += DecryptChar(txt[i], key[k_idx])
+                        c++
+                }
+	}
 
-func DecryptText(text string, key string){
-        result := ""
-}
-
-func EncryptText(text string, key string){
-        result := ""
+	return res
 }
--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	// Create cypher with key
-	// Find size of key
-	// Decrypt cypher
-	fmt.Println("Hello, World!")
+	fmt.Println("Enter ciphered text:")
+	reader := bufio.NewReader(os.Stdin)
+	cipherText, _ := reader.ReadString('\n')
+
+	fmt.Println("Ciphered Text:", cipherText)
+
+	probableKeyLengths := ProbKeyLen(cipherText)
+	fmt.Println("Probable Key Lengths:", probableKeyLengths)
+
+	keyLength := probableKeyLengths[0]
+	fmt.Println("Using Key Length:", keyLength)
+
+	transposedText := Transpose(cipherText, keyLength)
+
+	guessedKey := GuessKey(transposedText)
+	fmt.Println("Guessed Key:", guessedKey)
+
+	decryptedText := DecryptText(cipherText, guessedKey)
+	fmt.Println("Decrypted Text:", decryptedText)
 }
